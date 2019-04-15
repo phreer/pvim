e57e1f2dc977d408e7bd3acba3a220efd515394c
Phree Liu <iphreeliu@gmail.com>
2019-04-15 13:31:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pvim
+My personal vim configuration with c.vim, supertab, jedi-vim, NERDTree installed
---
+++ setup.sh
@@ -0,0 +1,4 @@
+git clone https://www.github.com/phreer/pvim.git
+cp pvim/.vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
